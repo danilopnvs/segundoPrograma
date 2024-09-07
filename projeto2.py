50c8fd557aa61544f173a788a4fdde8f3b075527
Danilo Neves <danilopereiranvs@gmail.com>
2024-09-07 12:16:42 -0300

    dia 07.09.2024

--- projeto2.py
+++ projeto2.py
@@ -18,31 +18,34 @@ class Pessoa:
     def imc_longo(self):
         IMC = self.peso/(self.altura*self.altura)
         if IMC < 18.5:
-            return "Abaixo do peso"
+            return "abaixo do peso"
         elif 18.5 <= IMC < 24.9:
-            return "Peso normal"
+            return "com o peso normal"
         elif 25 <= IMC < 29.9:
-            return "Sobrepeso"
+            return "em sobrepeso"
         else:
-            return "Obesidade"         
+            return "com obesidade"         
     
     # Método Apresentar
     def apresentar(self):
-        print("Eu sou", self.nome, "e tenho", self.idade, "anos")
-
+        return ("Eu sou {} e tenho {} anos".format(self.nome, self.idade))
+    
     # Método Compara idade
     def compara_idade(self, idade2):
         if self.idade>idade2:
-            print("Zezinho é mais velho")
+            return "é mais velho"
         elif self.idade<idade2:
-            print("Zezinho é mais novo")
+            return "é mais novo"
         elif self.idade==idade2:
-            print("Zezinho tem a mesma idade")
+            return "tem a mesma idade"
 
 pessoa1 = Pessoa("Zezinho", 15, 1.53, 68.2)
 
-print(pessoa1.eh_maior())
-print(pessoa1.imc())
-print(pessoa1.imc_longo())
-print(pessoa1.apresentar())
-print(pessoa1.compara_idade(15))
+print(pessoa1.nome,"é maior de idade?", pessoa1.eh_maior())
+print("O IMC do {} é {:.2f}".format(pessoa1.nome, pessoa1.imc()))
+print("O {} está {}".format(pessoa1.nome, pessoa1.imc_longo()))
+print("Quem eu sou? {}".format(pessoa1.apresentar()))
+
+
+idade_referencia = 10
+print ("{} tem {} anos, a segunda pessoa tem {} anos, portanto, {} {}".format(pessoa1.nome, pessoa1.idade, idade_referencia, pessoa1.nome,   pessoa1.compara_idade(idade_referencia)))
